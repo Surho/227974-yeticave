e0d3d3f2f7168c798356502a3241fcce306622da
Surho <surho007@hotmail.com>
2018-11-30 16:10:27 +0300

    schema.sql created

---
+++ schema.sql
@@ -0,0 +1,42 @@
+CREATE DATABASE yeticave;
+DEFAULT CHARACTER SET utf8;
+DEFAULT COLLATE utf8_general_ci;
+
+USE yeticave;
+
+CREATE TABLE  users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  registration_date datetime,
+  email CHAR(255) NOT NULL UNIQUE,
+  name CHAR(255) NOT NULL UNIQUE,
+  password CHAR(128),
+  avatar CHAR(128),
+  contacts CHAR(128)
+);
+
+CREATE TABLE lot (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  category_id INT NOT NULL,
+  user_id_winner INT NOT NULL,
+  user_id_author INT NOT NULL,
+  name CHAR(255) NOT NULL,
+  creation_date datetime,
+  end_date datetime,
+  about CHAR(255),
+  image CHAR(255),
+  initial_price INT,
+  step INT
+);
+
+CREATE TABLE bid (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  lot_id INT NOT NULL,
+  user_id INT NOT NULL,
+  date datetime,
+  sum_price INT
+);
+
+CREATE TABLE category (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  categories CHAR(255) NOT NULL UNIQUE
+);
