4b8dbbf12e0da439ff2f5d13957189ca9e3dd812
Surho <surho007@hotmail.com>
2018-12-03 21:54:19 +0300

    connected to database

--- index.php
+++ index.php
@@ -16,3 +16,17 @@ $layout = include_template('layout.php', [
 ]);
 
 print($layout);
+
+$con = mysqli_connect("localhost", "root", "sidrrdis12", "yeticave");
+
+if (!$con) {
+    echo "Ошибка: Невозможно установить соединение с MySQL." . PHP_EOL;
+    echo "Код ошибки errno: " . mysqli_connect_errno() . PHP_EOL;
+    echo "Текст ошибки error: " . mysqli_connect_error() . PHP_EOL;
+    exit;
+}
+
+echo "Соединение с MySQL установлено!" . PHP_EOL;
+echo "Информация о сервере: " . mysqli_get_host_info($con) . PHP_EOL;
+
+mysqli_close($con);
