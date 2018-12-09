a9a8e143723505354897484b4424553c93837e31
Surho <surho007@hotmail.com>
2018-12-09 13:06:59 +0300

    unused vars deleted

--- init.php
+++ init.php
@@ -1,3 +1,8 @@
 <?php
 $con = mysqli_connect("localhost", "root", "sidrrdis12", "yeticave");
 mysqli_set_charset($con, "utf-8");
+
+if(!$con) {
+    $error =  mysqli_error($con);
+    print('ошибка MySQL' . $error);
+}
--- lot.php
+++ lot.php
@@ -11,7 +11,6 @@ if(!empty($_GET['lot_id'])) {
     WHERE lot.id =' . intval($_GET['lot_id']);
     $result_sql = mysqli_query($con, $sql);
     $result_lot = mysqli_fetch_array($result_sql, MYSQLI_ASSOC);
-    var_dump($result_lot);
 
     if(!$result_lot) {
         navigate_to('pages/404.html');
@@ -24,13 +23,6 @@ if(!$result_sql) {
     $error =  mysqli_error($con);
     print('ошибка MySQL' . $error);
 }
-
-$lot_name = $result_lot['name'];
-$lot_pic = $result_lot['image'];
-$lot_description = $result_lot['description'];
-$lot_step = $result_lot['step'];
-$lot_price = $result_lot['price'];
-$lot_category = $result_lot['category_name'];
 
 $page_content = include_template('lot.php',[
     'lot_name' => $result_lot['name'],
